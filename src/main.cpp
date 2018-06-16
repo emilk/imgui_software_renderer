23e44fa1f52b6db3020c8a2b0fd09bfd23ba0ff4
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2018-06-16 18:14:16 +0200

    Add a test with a lot of text and graphics

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,64 @@
 
 using namespace emilib;
 
+void customRendering(ImVec4 col)
+{
+	// Taken from imgui_demo.cpp:
+	ImDrawList* draw_list = ImGui::GetWindowDrawList();
+	static float sz = 36.0f;
+	static float thickness = 4.0f;
+	const ImVec2 p = ImGui::GetCursorScreenPos();
+	const ImU32 col32 = ImColor(col);
+	float x = p.x + 4.0f, y = p.y + 4.0f, spacing = 8.0f;
+	for (int n = 0; n < 2; n++) {
+		float curr_thickness = (n == 0) ? 1.0f : thickness;
+		draw_list->AddCircle(ImVec2(x+sz*0.5f, y+sz*0.5f), sz*0.5f, col32, 20, curr_thickness); x += sz+spacing;
+		draw_list->AddRect(ImVec2(x, y), ImVec2(x+sz, y+sz), col32, 0.0f, ImDrawCornerFlags_All, curr_thickness); x += sz+spacing;
+		draw_list->AddRect(ImVec2(x, y), ImVec2(x+sz, y+sz), col32, 10.0f, ImDrawCornerFlags_All, curr_thickness); x += sz+spacing;
+		draw_list->AddRect(ImVec2(x, y), ImVec2(x+sz, y+sz), col32, 10.0f, ImDrawCornerFlags_TopLeft|ImDrawCornerFlags_BotRight, curr_thickness); x += sz+spacing;
+		draw_list->AddTriangle(ImVec2(x+sz*0.5f, y), ImVec2(x+sz,y+sz-0.5f), ImVec2(x,y+sz-0.5f), col32, curr_thickness); x += sz+spacing;
+		draw_list->AddLine(ImVec2(x, y), ImVec2(x+sz, y   ), col32, curr_thickness); x += sz+spacing; // Horizontal line (note: drawing a filled rectangle will be faster!)
+		draw_list->AddLine(ImVec2(x, y), ImVec2(x,    y+sz), col32, curr_thickness); x += spacing;    // Vertical line (note: drawing a filled rectangle will be faster!)
+		draw_list->AddLine(ImVec2(x, y), ImVec2(x+sz, y+sz), col32, curr_thickness); x += sz+spacing; // Diagonal line
+		draw_list->AddBezierCurve(ImVec2(x, y), ImVec2(x+sz*1.3f,y+sz*0.3f), ImVec2(x+sz-sz*1.3f,y+sz-sz*0.3f), ImVec2(x+sz, y+sz), col32, curr_thickness);
+		x = p.x + 4;
+		y += sz+spacing;
+	}
+	draw_list->AddCircleFilled(ImVec2(x+sz*0.5f, y+sz*0.5f), sz*0.5f, col32, 32); x += sz+spacing;
+	draw_list->AddRectFilled(ImVec2(x, y), ImVec2(x+sz, y+sz), col32); x += sz+spacing;
+	draw_list->AddRectFilled(ImVec2(x, y), ImVec2(x+sz, y+sz), col32, 10.0f); x += sz+spacing;
+	draw_list->AddRectFilled(ImVec2(x, y), ImVec2(x+sz, y+sz), col32, 10.0f, ImDrawCornerFlags_TopLeft|ImDrawCornerFlags_BotRight); x += sz+spacing;
+	draw_list->AddTriangleFilled(ImVec2(x+sz*0.5f, y), ImVec2(x+sz,y+sz-0.5f), ImVec2(x,y+sz-0.5f), col32); x += sz+spacing;
+	draw_list->AddRectFilled(ImVec2(x, y), ImVec2(x+sz, y+thickness), col32); x += sz+spacing;          // Horizontal line (faster than AddLine, but only handle integer thickness)
+	draw_list->AddRectFilled(ImVec2(x, y), ImVec2(x+thickness, y+sz), col32); x += spacing+spacing;     // Vertical line (faster than AddLine, but only handle integer thickness)
+	draw_list->AddRectFilled(ImVec2(x, y), ImVec2(x+1, y+1), col32);          x += sz;                  // Pixel (faster than AddLine)
+	draw_list->AddRectFilledMultiColor(ImVec2(x, y), ImVec2(x+sz, y+sz), IM_COL32(0,0,0,255), IM_COL32(255,0,0,255), IM_COL32(255,255,0,255), IM_COL32(0,255,0,255));
+	ImGui::Dummy(ImVec2((sz+spacing)*8, (sz+spacing)*3));
+}
+
+void showTestWindows()
+{
+	static ImVec4 s_some_color{ 0.7f, 0.8f, 0.9f, 0.5f };
+
+	ImGui::SetNextWindowPos(ImVec2{700.0f, 32.0f});
+	ImGui::SetNextWindowSize(ImVec2{400.0f, 800.0f});
+	if (ImGui::Begin("Graphics")) {
+		ImGui::ColorPicker4("some color", &s_some_color.x, ImGuiColorEditFlags_PickerHueBar);
+		ImGui::ColorPicker4("same color", &s_some_color.x, ImGuiColorEditFlags_PickerHueWheel);
+		customRendering(s_some_color);
+	}
+	ImGui::End();
+
+	ImGui::SetNextWindowPos(ImVec2{32.0f, 400.0f});
+	ImGui::SetNextWindowSize(ImVec2{600.0f, 600.0f});
+	if (ImGui::Begin("Test")) {
+		for (int i = 100; i > 0; --i) {
+			ImGui::Text("%d bottles of beer on the wall, %d bottles of beer. Take one down, pass it around, %d bottles of beer on the wall.", i, i, i - 1);
+		}
+	}
+	ImGui::End();
+}
+
 void run_software()
 {
 	int width_points = 1280;
@@ -43,6 +101,7 @@ void run_software()
 
 	LOG_F(INFO, "%dx%d points (%dx%d pixels)", width_points, height_points, width_pixels, height_pixels);
 
+	IMGUI_CHECKVERSION();
 	ImGui_SDL imgui_sdl(width_points, height_points, pixels_per_point);
 
 	auto surface = SDL_GetWindowSurface(window);
@@ -74,24 +133,34 @@ void run_software()
 		}
 		imgui_sdl.new_frame();
 
+		// --------------------------------------------------------------------
+
 		if (ImGui::BeginMainMenuBar()) {
 			imgui_helpers::show_im_gui_menu();
 			ImGui::EndMainMenuBar();
 		}
 
-		if (ImGui::Button("Tweak ImGui style for speed")) {
-			imgui_sw::make_style_fast();
-		}
+		ImGui::SetNextWindowPos(ImVec2{32.0f, 32.0f});
+		if (ImGui::Begin("Settings")) {
+			if (ImGui::Button("Tweak ImGui style for speed")) {
+				imgui_sw::make_style_fast();
+			}
 
-		ImGui::Checkbox("full_res", &full_res);
-		ImGui::Text("Paint time: %.2f ms", 1000 * paint_time);
-		if (!full_res) {
-			ImGui::Text("Upsample time: %.2f ms", 1000 * paint_time);
+			ImGui::Checkbox("full_res", &full_res);
+			ImGui::Text("Paint time: %.2f ms", 1000 * paint_time);
+			if (!full_res) {
+				ImGui::Text("Upsample time: %.2f ms", 1000 * paint_time);
+			}
+			imgui_sw::show_options(&sw_options);
+			imgui_sw::show_stats();
 		}
-		imgui_sw::show_options(&sw_options);
-		imgui_sw::show_stats();
+		ImGui::End();
+
+		showTestWindows();
+
+		// --------------------------------------------------------------------
+
 		imgui_sdl.paint();
-
 		double frame_paint_time;
 
 		if (full_res) {
@@ -156,6 +225,9 @@ void run_gl()
 			ImGui::EndMainMenuBar();
 		}
 		ImGui::Text("Paint time: %.2f ms", 1000 * paint_time);
+
+		showTestWindows();
+
 		imgui_sdl.paint();
 
 		glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
