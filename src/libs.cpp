27c1bcb797476d981daa875ad0389b9ab36f4fed
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2018-06-17 11:06:21 +0200

    Only compile specific parts of emilib for faster compile times

--- src/libs.cpp
+++ src/libs.cpp
@@ -8,4 +8,11 @@
 #define LOGURU_IMPLEMENTATION 1
 #include <loguru.hpp>
 
-#include <emilib/unity_build.cpp>
+#include <emilib/imgui_helpers.cpp>
+#include <emilib/imgui_sdl.cpp>
+#include <emilib/timer.cpp>
+
+// For gl path:
+#include <emilib/gl_lib.cpp>
+#include <emilib/gl_lib_sdl.cpp>
+#include <emilib/imgui_gl_lib.cpp>
