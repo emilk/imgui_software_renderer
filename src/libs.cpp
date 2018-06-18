b0bc495401eab29e055f009e8dcb9254b8774169
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2018-06-18 10:13:46 +0200

    Remove example dependency on configuru (unused)

--- src/libs.cpp
+++ src/libs.cpp
@@ -2,9 +2,6 @@
 #include <imgui/imgui_demo.cpp>
 #include <imgui/imgui_draw.cpp>
 
-#define CONFIGURU_IMPLEMENTATION 1
-#include <configuru.hpp>
-
 #define LOGURU_IMPLEMENTATION 1
 #include <loguru.hpp>
 
